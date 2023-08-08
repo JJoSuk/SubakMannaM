c25103b17f001a4719dae5e8751bcb041d6e9cda
JJoSuk <loveyou55042@gmail.com>
2023-08-08 11:51:09 +0900

    add chat config function

--- mannam-domain/src/main/java/kr/co/mannam/domain/config/webchat/WebChatConfig.java
+++ mannam-domain/src/main/java/kr/co/mannam/domain/config/webchat/WebChatConfig.java
@@ -2,7 +2,6 @@ package kr.co.mannam.domain.config.webchat;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
