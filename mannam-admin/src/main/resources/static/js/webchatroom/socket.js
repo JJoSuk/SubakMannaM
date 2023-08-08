fb2c19d36c3c727e384f80fd97fe3cc1bc6be3ae
JJoSuk <loveyou55042@gmail.com>
2023-08-08 11:49:29 +0900

    add chat screen configuration function

--- mannam-admin/src/main/resources/static/js/webchatroom/socket.js
+++ mannam-admin/src/main/resources/static/js/webchatroom/socket.js
@@ -6,7 +6,6 @@ document.write("<script\n" +
     "  integrity=\"sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=\"\n" +
     "  crossorigin=\"anonymous\"></script>")
 
-
 var usernamePage = document.querySelector('#username-page');
 var chatPage = document.querySelector('#chat-page');
 var usernameForm = document.querySelector('#usernameForm');
@@ -14,20 +13,19 @@ var messageForm = document.querySelector('#messageForm');
 var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
 var connectingElement = document.querySelector('.connecting');
-
 var stompClient = null;
 var username = null;
-
 var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
 
 // roomId 파라미터 가져오기
-const urlParams = new URLSearchParams(window.location.search);
-const roomId = urlParams.get('roomId');
+const url = new URL(location.href).searchParams;
+const roomId = url.get('roomId');
 
 function connect(event) {
+
     username = document.querySelector('#name').value.trim();
 
     // username 중복 확인
@@ -54,9 +52,7 @@ function onConnected() {
 
     // 서버에 username 을 가진 유저가 들어왔다는 것을 알림
     // /pub/chat/enterUser 로 메시지를 보냄
-    stompClient.send("/pub/chat/enterUser",
-        {},
-        JSON.stringify({
+    stompClient.send("/pub/chat/enterUser", {}, JSON.stringify({
             "roomId": roomId,
             sender: username,
             type: 'ENTER'
@@ -64,7 +60,6 @@ function onConnected() {
     )
 
     connectingElement.classList.add('hidden');
-
 }
 
 // 유저 닉네임 중복 확인
@@ -72,7 +67,7 @@ function isDuplicateName() {
 
     $.ajax({
         type: "GET",
-        url: "/put/chat/duplicateName",
+        url: "/chat/duplicateName",
         data: {
             "username": username,
             "roomId": roomId
@@ -81,13 +76,8 @@ function isDuplicateName() {
             console.log("함수 동작 확인 : " + data);
 
             username = data;
-
-            // username을 성공적으로 받아왔을 때, connect 함수 실행
-            // 오류 뜨면 지우는걸로
-            onConnected();
         }
     })
-
 }
 
 // 유저 리스트 받기
@@ -97,7 +87,7 @@ function getUserList() {
 
     $.ajax({
         type: "GET",
-        url: "/put/chat/userlist",
+        url: "/chat/userlist",
         data: {
             "roomId": roomId
         },
@@ -107,6 +97,7 @@ function getUserList() {
                 //console.log("data[i] : "+data[i]);
                 users += "<li class='dropdown-item'>" + data[i] + "</li>"
             }
+
             $list.html(users);
         }
     })
@@ -119,6 +110,7 @@ function onError(error) {
 
 // 메시지 전송때는 JSON 형식을 메시지를 전달한다.
 function sendMessage(event) {
+
     var messageContent = messageInput.value.trim();
 
     if (messageContent && stompClient) {
@@ -138,7 +130,7 @@ function sendMessage(event) {
 // 메시지를 받을 때도 마찬가지로 JSON 타입으로 받으며,
 // 넘어온 JSON 형식의 메시지를 parse 해서 사용한다.
 function onMessageReceived(payload) {
-    //console.log("payload 들어오냐? :"+payload);
+
     var chat = JSON.parse(payload.body);
 
     var messageElement = document.createElement('li');
@@ -147,12 +139,10 @@ function onMessageReceived(payload) {
         messageElement.classList.add('event-message');
         chat.content = chat.sender + chat.message;
         getUserList();
-
     } else if (chat.type === 'EXIT') { // chatType 가 Exit 라면 아래 내용
         messageElement.classList.add('event-message');
         chat.content = chat.sender + chat.message;
         getUserList();
-
     } else { // chatType 이 talk 라면 아래 내용용
         messageElement.classList.add('chat-message');
 
@@ -169,26 +159,128 @@ function onMessageReceived(payload) {
         messageElement.appendChild(usernameElement);
     }
 
-    var textElement = document.createElement('p');
-    var messageText = document.createTextNode(chat.message);
-    textElement.appendChild(messageText);
+    var contentElement = document.createElement('p');
 
-    messageElement.appendChild(textElement);
+    // 만약 s3DataUrl 의 값이 null 이 아니라면 => chat 내용이 파일 업로드와 관련된 내용이라면
+    // img 를 채팅에 보여주는 작업
+    if(chat.s3DataUrl != null){
+        var imgElement = document.createElement('img');
+        imgElement.setAttribute("src", chat.s3DataUrl);
+        imgElement.setAttribute("width", "300");
+        imgElement.setAttribute("height", "300");
+
+        var downBtnElement = document.createElement('button');
+        downBtnElement.setAttribute("class", "btn fa fa-download");
+        downBtnElement.setAttribute("id", "downBtn");
+        downBtnElement.setAttribute("name", chat.fileName);
+        downBtnElement.setAttribute("onclick", `downloadFile('${chat.fileName}', '${chat.fileDir}')`);
+
+        contentElement.appendChild(imgElement);
+        contentElement.appendChild(downBtnElement);
+    }else{
+        // 만약 s3DataUrl 의 값이 null 이라면
+        // 이전에 넘어온 채팅 내용 보여주기기
+        var messageText = document.createTextNode(chat.message);
+        contentElement.appendChild(messageText);
+    }
+
+    messageElement.appendChild(contentElement);
 
     messageArea.appendChild(messageElement);
     messageArea.scrollTop = messageArea.scrollHeight;
 }
-
 
 function getAvatarColor(messageSender) {
     var hash = 0;
     for (var i = 0; i < messageSender.length; i++) {
         hash = 31 * hash + messageSender.charCodeAt(i);
     }
-
     var index = Math.abs(hash % colors.length);
     return colors[index];
 }
 
 usernameForm.addEventListener('submit', connect, true)
 messageForm.addEventListener('submit', sendMessage, true)
+
+//--------------------------파일 업로드 관련
+function uploadFile(){
+    var file = $("#file")[0].files[0];
+    var formData = new FormData();
+    formData.append("file",file);
+    formData.append("roomId", roomId);
+
+    // 확장자 추출
+    var fileDot = file.name.lastIndexOf(".");
+
+    // 확장자 검사
+    var fileType = file.name.substring(fileDot + 1, file.name.length);
+    // console.log("type : " + fileType);
+
+    if (!(fileType == "png" || fileType == "jpg" || fileType == "jpeg" || fileType == "gif"))
+    {
+        alert("파일 업로드는 png, jpg, gif, jpeg 만 가능합니다");
+        return;
+    }
+
+    // ajax 로 multipart/form-data 를 넘겨줄 때는
+    //         processData: false,
+    //         contentType: false
+    // 처럼 설정해주어야 한다.
+
+    // 동작 순서
+    // post 로 rest 요청한다.
+    // 1. 먼저 upload 로 파일 업로드를 요청한다.
+    // 2. upload 가 성공적으로 완료되면 data 에 upload 객체를 받고,
+    // 이를 이용해 chatMessage 를 작성한다.
+    $.ajax({
+        type : 'POST',
+        url : '/s3/upload',
+        data : formData,
+        processData: false,
+        contentType: false
+    }).done(function (data){
+        // console.log("업로드 성공")
+
+        var chatMessage = {
+            "roomId": roomId,
+            sender: username,
+            message: username+"님의 파일 업로드",
+            type: 'TALK',
+            s3DataUrl : data.s3DataUrl, // Dataurl
+            "fileName": file.name, // 원본 파일 이름
+            "fileDir": data.fileDir // 업로드 된 위치
+        };
+
+        // 해당 내용을 발신한다.
+        stompClient.send("/pub/chat/sendMessage", {}, JSON.stringify(chatMessage));
+    }).fail(function (error){
+        alert(error);
+    })
+}
+
+//--------------------------파일 다운로드 부분
+// 버튼을 누르면 downloadFile 메서드가 실행됨
+// 다운로드 url 은 /s3/download+원본파일이름
+function downloadFile(name, dir){
+
+    let url = "/s3/download/"+name;
+
+    // get 으로 rest 요청한다.
+    $.ajax({
+        url: "/s3/download/"+name, // 요청 url 은 download/{name}
+        data: {
+            "fileDir" : dir // 파일의 경로를 파라미터로 넣는다.
+        },
+        dataType: 'binary', // 파일 다운로드를 위해서는 binary 타입으로 받아야한다.
+        xhrFields: {
+            'responseType': 'blob' // 여기도 마찬가지
+        },
+        success: function(data) {
+
+            var link = document.createElement('a');
+            link.href = URL.createObjectURL(data);
+            link.download = name;
+            link.click();
+        }
+    });
+}
