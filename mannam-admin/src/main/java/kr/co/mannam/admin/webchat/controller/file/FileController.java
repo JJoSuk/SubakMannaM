b297c83d43f731e1f96d9ce239d3b4fd6c484438
JJoSuk <loveyou55042@gmail.com>
2023-08-15 18:24:59 +0900

    Chatroom related actions - Save File

--- mannam-admin/src/main/java/kr/co/mannam/admin/webchat/controller/file/FileController.java
+++ mannam-admin/src/main/java/kr/co/mannam/admin/webchat/controller/file/FileController.java
@@ -1,12 +1,12 @@
 package kr.co.mannam.admin.webchat.controller.file;
 
-import kr.co.mannam.admin.webchat.dto.file.FileS3DTO;
-import kr.co.mannam.admin.webchat.service.file.S3FileService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import kr.co.mannam.admin.webchat.dto.file.FileUploadDto;
+import kr.co.mannam.admin.webchat.service.file.S3FileService;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -19,30 +19,22 @@ public class FileController {
     @Autowired
     private S3FileService fileService;
 
-    /**
-     * 프론트에서 ajax 를 통해 /upload 로 MultipartFile 형태로 파일과 roomId 를 전달 받고, file 을 s3File 메서드를 통해 업로드한다.
-     * @param file 파일 업로드
-     * @param roomId 채팅방 아이디
-     * @return
-     */
+    // 프론트에서 ajax 를 통해 /upload 로 MultipartFile 형태로 파일과 roomId 를 전달받는다.
+    // 전달받은 file 를 uploadFile 메서드를 통해 업로드한다.
     @PostMapping("/upload")
-    public FileS3DTO s3File(@RequestParam("file") MultipartFile file, @RequestParam("roomId")String roomId){
+    public FileUploadDto uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("roomId")String roomId){
 
-        FileS3DTO fileReq = fileService.s3File(file, UUID.randomUUID().toString(), roomId);
+        FileUploadDto fileReq = fileService.uploadFile(file, UUID.randomUUID().toString(), roomId);
         log.info("최종 upload Data {}", fileReq);
 
         // fileReq 객체 리턴
         return fileReq;
     }
 
-    /**
-     * 클라이언트가 요청한 파일을 찾아서 다운로드할 수 있도록 HTTP 응답을 반환하는 역할
-     * @param fileName 다운로드할 파일 이름
-     * @param fileDir S3 버킷 내의 경로
-     * @return
-     */
+    // get 으로 요청이 오면 아래 download 메서드를 실행한다.
+    // fileName 과 파라미터로 넘어온 fileDir 을 getObject 메서드에 매개변수로 넣는다.
     @GetMapping("/download/{fileName}")
-    public ResponseEntity<byte[]> download(@PathVariable String fileName, @RequestParam("fileDir")String fileDir) {
+    public ResponseEntity<byte[]> download(@PathVariable String fileName, @RequestParam("fileDir")String fileDir){
         log.info("fileDir : fileName [{} : {}]", fileDir, fileName);
         try {
             // 변환된 byte, httpHeader 와 HttpStatus 가 포함된 ResponseEntity 객체를 return 한다.
@@ -51,4 +43,5 @@ public class FileController {
             throw new RuntimeException(e);
         }
     }
+
 }
--- mannam-admin/src/main/java/kr/co/mannam/admin/webchat/service/file/S3FileService.java
+++ mannam-admin/src/main/java/kr/co/mannam/admin/webchat/service/file/S3FileService.java
@@ -4,12 +4,10 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 import com.amazonaws.services.s3.transfer.Upload;
 import com.amazonaws.util.IOUtils;
-import kr.co.mannam.admin.webchat.dto.file.FileS3DTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,43 +17,41 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import kr.co.mannam.admin.webchat.dto.file.FileUploadDto;
+import kr.co.mannam.domain.entity.webchat.ChatFile;
+import kr.co.mannam.domain.entity.webchat.ChatRoom;
+import kr.co.mannam.domain.repository.webchat.ChatFileRepository;
+import kr.co.mannam.domain.repository.webchat.ChatRoomRepository;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class S3FileService implements FileService{
+public class S3FileService implements FileService {
 
-    /**
-     * AmazonS3 주입받기
-     */
+    // AmazonS3 주입받기
     private final AmazonS3 amazonS3;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatFileRepository chatFileRepository;
 
-    /**
-     * application-s3.properties 에서 S3 bucket 이름 가져옴
-     */
+    // S3 bucket 이름
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    /**
-     * application-s3.properties 에서 S3 base url 가져옴
-     */
+    // S3 base URL
     @Value("${cloud.aws.s3.bucket.url}")
     private String baseUrl;
 
-    /**
-     * s3File = MultipartFile 과 transaction, roomId
-     * @param file 파일 업로드
-     * @param transaction 랜덤한 파일 위치 생성
-     * @param roomId 채팅방 아이디
-     * @return
-     */
+    // MultipartFile 과 transcation, roomId 를 전달받는다.
+    // 이때 transcation 는 파일 이름 중복 방지를 위한 UUID 를 의미한다.
     @Override
-    public FileS3DTO s3File(MultipartFile file, String transaction, String roomId) {
+    public FileUploadDto uploadFile(MultipartFile file, String transaction, String roomId) {
         try{
-            String filename = file.getOriginalFilename(); // 파일 원본 이름
+
+            String filename = file.getOriginalFilename(); // 파일원본 이름
             String key = roomId+"/"+transaction+"/"+filename; // S3 파일 경로
 
             // 매개변수로 넘어온 multipartFile 을 File 객체로 변환 시켜서 저장하기 위한 메서드
@@ -75,16 +71,27 @@ public class S3FileService implements FileService{
             removeFile(convertedFile);
 
             // uploadDTO 객체 빌드
-            FileS3DTO s3FileReq = FileS3DTO.builder()
+            FileUploadDto uploadReq = FileUploadDto.builder()
                     .transaction(transaction)
-                    .webChatRoom(roomId)
+                    .chatRoom(roomId)
                     .originFileName(filename)
                     .fileDir(key)
-                    .s3DataUrl(baseUrl+"/"+key)
+                    .s3DataUrl(baseUrl + "/" + key)
                     .build();
 
-            // uploadDTO 객체 리턴
-            return s3FileReq;
+            // 데이터베이스에 파일 정보 저장
+            ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElse(null);
+            if (chatRoom != null) {
+                ChatFile chatFile = ChatFile.builder()
+                        .s3Key(key)
+                        .fileName(filename)
+                        .chatRoom(chatRoom)
+                        .build();
+                chatFileRepository.save(chatFile);
+            }
+
+            // uploadDTO 객체 리턴, 응답 변환
+            return uploadReq;
 
         } catch (Exception e) {
             log.error("fileUploadException {}", e.getMessage());
@@ -92,46 +99,57 @@ public class S3FileService implements FileService{
         }
     }
 
-    /**
-     * s3 에 저장된 파일들은 : roomId/UUID(파일명)/원본 파일명 으로 변경된다.
-     * 그래서 path 를 사용해 파일을 삭제하기 위해서 path -> roomId 로 자동변환 되어
-     * roomId 를 입력하면 입력된 roomId 에 저장된 파일들은 모두 삭제된다.
-     * @param path deleteFileDir(해당 채팅방 모든 파일 삭제)
-     */
+    // path 아래있는 모든 파일을 삭제한다.
+// 이때 path 는 roomId 가 된다 => S3 에 roomId/변경된 파일명(uuid)/원본 파일명 으로 되어있기 때문에
+// roomId 를 적어주면 기준이 되는 roomId 아래의 모든 파일이 삭제된다.
     @Override
-    public void deleteFileDir(String path) {
-        for (S3ObjectSummary summary : amazonS3.listObjects(bucket, path).getObjectSummaries()) {
-            amazonS3.deleteObject(bucket, summary.getKey());
+    public void deleteFileDir(String roomId) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElse(null);
+        if (chatRoom != null) {
+            List<ChatFile> files = chatFileRepository.findByChatRoom(chatRoom);
+            for (ChatFile file : files) {
+                try {
+                    log.info("Attempting to delete file with S3 key: " + file.getS3Key());
+                    amazonS3.deleteObject(bucket, file.getS3Key());
+                    chatFileRepository.delete(file);
+                    log.info("Successfully deleted file with S3 key: " + file.getS3Key());
+                } catch (Exception e) {
+                    log.error("Error deleting file with S3 key: " + file.getS3Key(), e);
+                }
+            }
+        } else {
+            log.warn("ChatRoom with ID " + roomId + " not found.");
         }
     }
 
-    /**
-     * byte 배열 타입을 return 한다.
-     * @param fileDir 파일이 저장된 경로
-     * @param fileName 가져올 파일 이름
-     * @return
-     */
     @Override
     public ResponseEntity<byte[]> getObject(String fileDir, String fileName) throws IOException {
         // bucket 와 fileDir 을 사용해서 S3 에 있는 객체 - object - 를 가져온다.
         S3Object object = amazonS3.getObject(new GetObjectRequest(bucket, fileDir));
 
-        // object 를 S3ObjectInputStream 형태로 변환
+        // object 를 S3ObjectInputStream 형태로 변환한다.
         S3ObjectInputStream objectInputStream = object.getObjectContent();
 
-        // 이후 다시 byte 배열 형태로 변환
-        // 아마도 파일 전송을 위해서는 다시 byte[] 즉, binary 로 변환해서 전달해야햐기 때문
+        // 이후 다시 byte 배열 형태로 변환한다.
+        // 아마도 파일 다운로드를 위해서는 byte 형태로 변환할 필요가 있어서 그런듯하다
         byte[] bytes = IOUtils.toByteArray(objectInputStream);
 
-        // 여기는 httpHeader 에 파일 다운로드 요청을 하기 위한 내용
+        // 여기는 httpHeader 에 파일 다운로드 요청을 하기 위한내용
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
 
         // 지정된 fileName 으로 파일이 다운로드 된다.
         httpHeaders.setContentDispositionFormData("attachment", fileName);
 
-        // 주어진 경로와 파일 이름을 가지고 s3 에서 파일을 가져와 http 응답으로 반환하는 역할.
-        // 파일 내용을 byte 변환 -> http 응답 헤더를 설정하여 파일 다운 정보 제공 -> 파일 내용과 헤더를 포함한 http 응답을 반환
+        log.info("HttpHeader : [{}]", httpHeaders);
+
+        // 최종적으로 ResponseEntity 객체를 리턴하는데
+        // --> ResponseEntity 란?
+        // ResponseEntity 는 사용자의 httpRequest 에 대한 응답 테이터를 포함하는 클래스이다.
+        // 단순히 body 에 데이터를 포함하는 것이 아니라, header 와 httpStatus 까지 넣어 줄 수 있다.
+        // 이를 통해서 header 에 따라서 다른 동작을 가능하게 할 수 있다 => 파일 다운로드!!
+
+        // 나는 object가 변환된 byte 데이터, httpHeader 와 HttpStatus 가 포함된다.
         return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
     }
 }
