74ae5536d0f520192a494a7d8a256674a5a60301
JJoSuk <loveyou55042@gmail.com>
2023-08-15 18:26:22 +0900

    Uploading chatroom-related files

--- mannam-admin/src/main/java/kr/co/mannam/admin/webchat/dto/file/FileUploadDto.java
+++ mannam-admin/src/main/java/kr/co/mannam/admin/webchat/dto/file/FileUploadDto.java
@@ -6,22 +6,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class FileS3DTO {
+@Data
+@Builder
+public class FileUploadDto {
 
-    private MultipartFile file; // 파일 업로드
+    private MultipartFile file; // MultipartFile
 
     private String originFileName; // 파일 원본 이름
 
     private String transaction; // UUID 를 활용한 랜덤한 파일 위치
 
-    private String webChatRoom; // 파일이 올라간 채팅방 ID
+    private String chatRoom; // 파일이 올라간 채팅방 ID
 
     private String s3DataUrl; // 파일 링크
 
     private String fileDir; // S3 파일 경로
-
 }
